168fd3b0d9ccee85f2713833577936df549da728
Prama Ray <pramray300306@gmail.com>
2025-07-16 18:31:20 +0530

    Minor changes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 const cors = require('cors');
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }))
 
 // Test routes
 const apiRoutes = require('./routes/api');
