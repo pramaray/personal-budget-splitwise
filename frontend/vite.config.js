b8ec57a0da93c960fcca719b2cb784e29c93ccc4
Prama Ray <pramray300306@gmail.com>
2025-07-16 12:07:24 +0530

    Made changes to CSS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react(),tailwindcss()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  }
-=======
-  plugins: [react(), tailwindcss()],
->>>>>>> b1ce07c00e1dfd75fe32cf8b246fa7f01fca55fa
 })
