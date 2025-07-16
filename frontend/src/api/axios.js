b9923f872ec363071e350484439c3ed747f05fa1
Prama Ray <pramray300306@gmail.com>
2025-07-16 18:17:07 +0530

    Minor changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // backend base URL
+  baseURL:import.meta.env.VITE_BACKEND_URL, // backend base URL
   withCredentials: false,               // keep false unless using cookies
 });
 
